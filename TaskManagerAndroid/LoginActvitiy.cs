7f481c4cc46ad718353f85b00aa56bf5397f5b3d
Dan Stow <Dan_Stow@live.co.uk>
2018-04-29 17:50:47 +0100

    Fixed references issues and allow user to login

--- TaskManagerAndroid/LoginActvitiy.cs
+++ TaskManagerAndroid/LoginActvitiy.cs
@@ -10,6 +10,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+using API = TaskManagerAPI;
+
 namespace TaskManagerAndroid
 {
     [Activity(Label = "Task Manager Login", MainLauncher = true)]
@@ -21,6 +23,35 @@ namespace TaskManagerAndroid
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Login);
+
+            // Setup events for the two buttons
+            FindViewById<Button>(Resource.Id.btnLogin).Click += btnLogin_Click;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Get email and password
+            string email, password;
+            EditText txtEmail, txtPassword;
+            txtEmail = FindViewById<EditText>(Resource.Id.txtLoginEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtLoginPassword);
+            email = txtEmail.Text;
+            password = txtPassword.Text;
+            
+            // ToDo: Validate these details
+
+            string token = null;
+            if(API.Account.GetAccessToken(email, password, out token))
+            {
+                // Clear down and move to view projects activity
+                txtEmail.Text = "";
+                txtPassword.Text = "";
+
+            }
+            else
+            {
+                // Show credentials error
+            }
         }
     }
 }
--- TaskManagerAndroid/Resources/Resource.Designer.cs
+++ TaskManagerAndroid/Resources/Resource.Designer.cs
@@ -44,14 +44,11 @@ namespace TaskManagerAndroid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f040006
+			public const int btnCreateNewAccount = 2130968582;
+			
 			// aapt resource value: 0x7f040005
-			public const int button1 = 2130968581;
-			
-			// aapt resource value: 0x7f040002
-			public const int editText1 = 2130968578;
-			
-			// aapt resource value: 0x7f040004
-			public const int editText2 = 2130968580;
+			public const int btnLogin = 2130968581;
 			
 			// aapt resource value: 0x7f040000
 			public const int textView1 = 2130968576;
@@ -61,6 +58,12 @@ namespace TaskManagerAndroid
 			
 			// aapt resource value: 0x7f040003
 			public const int textView3 = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int txtLoginEmail = 2130968578;
+			
+			// aapt resource value: 0x7f040004
+			public const int txtLoginPassword = 2130968580;
 			
 			static Id()
 			{
@@ -80,6 +83,9 @@ namespace TaskManagerAndroid
 			
 			// aapt resource value: 0x7f020001
 			public const int Main = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Register = 2130837506;
 			
 			static Layout()
 			{
