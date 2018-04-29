526a3ab24ccee70c13a347d5ebc176e67dc4876b
Dan Stow <Dan_Stow@live.co.uk>
2018-04-29 20:40:34 +0100

    Add project API controller with calls to get and create a new project

---
+++ TaskManagerWeb/Controllers/ProjectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskManagerWeb.Models;
+
+namespace TaskManagerWeb.Controllers
+{
+    public class ProjectController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ProjectController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public Project[] GetProjects()
+        {
+            return _context.Projects.Where(x => x.CreatedBy == User.Identity.Name).ToArray();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public int CreateProject(string name)
+        {
+            Project newProj = new Project()
+            {
+                Name = name,
+                CreatedBy = User.Identity.Name,
+                CreatedWhen = DateTime.Now
+            };
+
+            _context.Projects.Add(newProj);
+            _context.SaveChanges();
+
+            return newProj.ProjectId;
+        }
+    }
+}
