632a28f8e31c5d799e0d5fccf35cc21d68d3dbff
Dan Stow <Dan_Stow@live.co.uk>
2018-04-29 16:53:51 +0100

    Update connection string and change project to use IIS

--- TaskManagerWeb/Models/IdentityModels.cs
+++ TaskManagerWeb/Models/IdentityModels.cs
@@ -22,7 +22,7 @@ namespace TaskManagerWeb.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("TaskManagerDevConn", throwIfV1Schema: false)
         {
         }
         
