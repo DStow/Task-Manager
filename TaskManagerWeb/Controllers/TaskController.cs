30189c06f2873fa6151d3060b8cb9cd370f16fcb
Dan Stow <Dan_Stow@live.co.uk>
2018-04-30 15:07:17 +0100

    Add function for creating a new project task

--- TaskManagerWeb/Controllers/TaskController.cs
+++ TaskManagerWeb/Controllers/TaskController.cs
@@ -22,7 +22,7 @@ namespace TaskManagerWeb.Controllers
         {
             // Check user is the project creator
             var project = _context.Projects.Where(x => x.ProjectId == projectId).FirstOrDefault();
-            if(project.CreatedBy.ToLower() == User.Identity.Name.ToLower())
+            if (project.CreatedBy.ToLower() == User.Identity.Name.ToLower())
             {
                 Models.ProjectTask[] tasks = _context.Tasks.Where(x => x.ProjectId == projectId).ToArray();
                 return tasks;
@@ -32,5 +32,42 @@ namespace TaskManagerWeb.Controllers
                 throw new Exception("Project does not belong to this user!");
             }
         }
+
+        [Authorize]
+        [HttpPost]
+        public int CreateTask(CreateTaskBindingClass newTask)
+        {
+            // Check project belongs to user
+            Models.Project project = _context.Projects.Where(x => x.ProjectId == newTask.ProjectId && x.CreatedBy.ToLower() == User.Identity.Name.ToLower()).FirstOrDefault();
+
+            if (project == null)
+            {
+                throw new Exception("Project does not exist!");
+            }
+            else
+            {
+                Models.ProjectTask dbTask = new Models.ProjectTask()
+                {
+                    Completed = false,
+                    CreatedBy = User.Identity.Name,
+                    CreatedWhen = DateTime.Now,
+                    Description = newTask.Description,
+                    ProjectId = newTask.ProjectId
+                };
+
+                _context.Tasks.Add(dbTask);
+                _context.SaveChanges();
+
+                return dbTask.ProjectTaskId;
+            }
+        }
+
+        public class CreateTaskBindingClass
+        {
+            public int ProjectId { get; set; }
+            public string Description { get; set; }
+        }
     }
+
+
 }
