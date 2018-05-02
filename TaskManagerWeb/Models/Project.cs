499e75f3aaa52cc1530bd02ab5e3dc2129d898ce
Dan Stow <Dan_Stow@live.co.uk>
2018-05-02 22:36:39 +0100

    Revert project changes

--- TaskManagerWeb/Models/Project.cs
+++ TaskManagerWeb/Models/Project.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -12,22 +11,5 @@ namespace TaskManagerWeb.Models
         public string Name { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedWhen { get; set; }
-
-        public ICollection<ProjectTask> Tasks { get; set; }
-
-        [NotMapped]
-        public DateTime? LatestEditDate { get { return CalculateLatestEditDate(); } }
-
-        public DateTime? CalculateLatestEditDate()
-        {
-            DateTime? result = null;
-
-            if(Tasks != null)
-            {
-                result = Tasks.OrderByDescending(x => x.CreatedWhen).First().CreatedWhen;
-            }
-
-            return result;
-        }
     }
 }
