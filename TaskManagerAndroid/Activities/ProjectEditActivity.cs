addeb5bcf6c14695a562d5af9ab74d7afb11c143
Dan Stow <Dan_Stow@live.co.uk>
2018-05-13 19:54:25 +0100

    Add new activity for editing a project

---
+++ TaskManagerAndroid/Activities/ProjectEditActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TaskManagerAndroid.Activities
+{
+    [Activity(Label = "ProjectEditActivity")]
+    public class ProjectEditActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Create your application here
+            this.SetContentView(Resource.Layout.ProjectCreate);
+
+            // Set click on the create button
+            FindViewById<Button>(Resource.Id.btnCreateProject).Click += btnCreateProject_Click;
+        }
+
+        private void btnCreateProject_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
